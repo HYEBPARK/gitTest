9da569e44218369b07b679c70f5257f423f855ff
hyebpark <hyeb@hyebparkui-MacBookPro.local>
2022-06-27 01:07:55 +0900

    b branch 입니다.

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("base1 입니다.");
 		System.out.println("base2 입니다.");
+		System.out.println("b branch 입니다.");
 	}
 }
